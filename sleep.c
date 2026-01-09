c920f74a6609f8db3761654639ec85376d885291
BiroByte <yousefalshikhali1711@gmail.com>
2026-01-09 12:11:06 +0300

    all of code !

--- sleep.c
+++ sleep.c
@@ -1,6 +1,48 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
+#define SYS_write 1 
+#define SYS_exit 60
+#define SYS_nanosleep 35 
+
+typedef struct timespec {
+	long tv_sec; 
+	long tv_nsec; 
+} timespec;
+
+// we named this function syscall1 because 1 represent the number of args this function takes 
+// if this function takes 2 arguments then we would call it syscall2 and so on until we reach 6.
+long syscall1(long number, long arg1){
+		long result; 
+		__asm__ __volatile__(
+			"syscall"
+			: "=a" (result)
+			: "a" (number), "D" (arg1)
+			: "rcx", "r11", "memory"
+				);
+
+		return result;
+}
+long syscall2(long number, long arg1, long arg2){
+		long result; 
+		__asm__ __volatile__(
+			"syscall"
+			: "=a" (result)
+			: "a" (number), "D" (arg1), "S" (arg2)
+			: "rcx", "r11", "memory"
+				);
+
+		return result;
+}
+long syscall3(long number, long arg1, long arg2, long arg3){
+		long result; 
+		__asm__ __volatile__(
+			"syscall"
+			: "=a" (result)
+			: "a" (number), "D" (arg1), "S" (arg2), "d" (arg3)
+			: "rcx", "r11", "memory"
+				);
+
+		return result;
+}
+
 
 int parse_int(char *raw_int){
   int result = 0; 
@@ -14,46 +56,57 @@ int parse_int(char *raw_int){
   return result; 
 }
 
+long unsigned strlen(char *string){
+	char *cursor = string;
+	while(*cursor){
+		cursor++;
+	}
+	long unsigned result = cursor - string;
+	return result;
+}
 
+
+int print(char *string){
+	syscall3(SYS_write,1, (long) string , strlen(string));
+}
+
+void sleep(long seconds){
+	timespec duration = {0};
+	duration.tv_sec = seconds; 
+	syscall2(SYS_nanosleep, (long)(&duration) , 0);	
+}
 
 int main(int argc , char *argv[]){
   if (argc != 2) {
-    printf("Usage: sleep NUMBER\n Pause for NUMBER seconds \n");
+    print("Usage: sleep NUMBER\n Pause for NUMBER seconds \n");
     return 1;
   }
   char *raw_seconds = argv[1];
   long seconds = parse_int(raw_seconds);
 
-  printf("sleeping for %d seconds \n",seconds);
+  print("sleeping for ");
+  print(raw_seconds);
+  print(" seconds \n");
   sleep(seconds);
   return 0;
 }
 
-void _start(){
-  long rsp;
+void exit(int code){
+	syscall1(SYS_exit, code);
 
-  __asm__ __volatile__("mov %%rsp, %0" : "=r" (rsp));
-  long argc = *(long *)rsp; 
-  char **argv = (char **)(rsp + 8);
-
-  main((int)argc , argv);
+	for(;;) {}
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+__attribute__((naked)) void _start(){
+  __asm__ __volatile__(
+		  "xor %ebp, %ebp\n"
+		  "mov (%rsp), %rdi\n"
+		  "lea 8(%rsp), %rsi\n"
+		  "and $-16, %rsp\n"
+		  "call main\n"
+		  "mov %rax, %rdi\n"
+		  "call exit\n"
+	);
+}
